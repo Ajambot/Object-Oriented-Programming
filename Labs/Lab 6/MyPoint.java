5c8d8a2ada38e5b25196d2d25bc7c322c5ab24a6
Martin Morales <50452065+Ajambot@users.noreply.github.com>
2023-02-15 18:34:26 -0500

    Lab 6

---
+++ Labs/Lab 6/MyPoint.java
@@ -0,0 +1,30 @@
+// MyPoint class with attributes and methods described in main
+public class MyPoint {
+    private int x, y;
+
+    // Constructors
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters and setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
